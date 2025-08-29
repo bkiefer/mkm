5fce78c5ac3c547f5392d13f82df6b90307c9763
Bernd Kiefer <j_a_w_bone@gmx.de>
2025-08-29 19:52:16 +0200

    Add necessary submodules and central build script

---
+++ build_modules.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#set -x
+scrdir=`dirname $0`
+cd $scrdir
+# check out and update all modules
+git pull --recurse-submodules
+git submodule update --init --recursive --remote
+
+# ASR and speaker identification
+pushd asrident
+./build_docker.sh
+# download silero, speaker identification and whisper models
+./model_download.sh
+popd
+
+# Build docker for intent and slot recognition
+pushd drz_intentslot
+./model_download.sh
+./build_docker.sh
+popd
+
+# Make sure VOnDA compiler is available
+pushd vonda
+#git submodule init; git pull --recurse-submodules # do we need that?
+mvn install
+popd
+
+# Compile the MKM and build the MKM docker
+mvn clean
+./vonda/bin/vondac -c "config.yml"
+mvn install
+./build_docker.sh
+popd
