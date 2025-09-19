b8c8d2f676ef609759e0425701801a78236b175a
Bernd Kiefer <bernd.kiefer@dfki.de>
2025-09-19 17:20:23 +0200

    use latest modules

--- prepare_localinst.sh
+++ prepare_localinst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -x
+set -xv
 scrdir=`dirname $0`
 cd $scrdir
 # check out and update all modules
@@ -8,10 +8,12 @@ git submodule update --init --recursive --remote
 
 # ASR and speaker identification
 pushd modules/asrident
+echo
 uv sync
-. .venv/bin/activate
+uv -v pip install whisper-gstreamer/
 # download silero and speaker identification and whisper models
-./model_download.sh
+./model_download.sh -l
+uv run download_models.py large-v3-turbo
 popd
 
 # Build docker for intent and slot recognition
@@ -22,12 +24,13 @@ popd
 
 # Make sure VOnDA compiler is available
 pushd modules/vonda
-#git submodule init; git pull --recurse-submodules # do we need that?
-mvn install
+if test -z $(find -name 'vonda*compiler.jar'); then
+    #git submodule init; git pull --recurse-submodules # do we need that?
+    mvn install
+fi
 popd
 
 # Compile the MKM and build the MKM docker
 mvn clean
 ./modules/vonda/bin/vondac -c "config.yml"
 mvn install
-popd
