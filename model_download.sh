ae5b2398324ff22fb04847b02e4cd331eb96b29d
Bernd Kiefer <bernd.kiefer@dfki.de>
2025-11-05 08:46:46 +0100

    new rasa model

--- model_download.sh
+++ model_download.sh
@@ -2,7 +2,8 @@
 #set -x
 scrdir=`dirname "$0"`
 cd "$scrdir"
-link='https://cloud.dfki.de/owncloud/index.php/s/SEyqYJXCaeHfdbJ/download/nlu-20250310-114133-several-kicker.tar.gz'
+link='https://cloud.dfki.de/owncloud/index.php/s/CPiKNWG62wJPFqs/download/nlu-20251103-142306-weighted-comptroller.tar.gz'
+#link='https://cloud.dfki.de/owncloud/index.php/s/SEyqYJXCaeHfdbJ/download/nlu-20250310-114133-several-kicker.tar.gz'
 if test -f rasa/models/"${link##*/}"; then
     :
 else
