fd2281126f151d7ab8a730f041ef3b808e94f574
Bernd Kiefer <j_a_w_bone@gmx.de>
2026-08-07 17:54:01 +0200

    correct way to use module branch

--- update_repo.sh
+++ update_repo.sh
@@ -8,5 +8,5 @@ fi
 # check out and update all modules
 git pull --recurse-submodules
 git submodule update --init --recursive --remote
-cd modules/asrident
-git checkout drz
+#cd modules/asrident
+#git checkout drz
